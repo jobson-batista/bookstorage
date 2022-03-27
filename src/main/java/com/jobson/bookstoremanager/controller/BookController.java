e35d492403ed03c0e12a91be4b45e52d52a5141c
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-03-27 00:55:57 -0300

    BookController created

---
+++ src/main/java/com/jobson/bookstoremanager/controller/BookController.java
@@ -0,0 +1,10 @@
+package com.jobson.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+}
