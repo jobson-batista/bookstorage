10e7fcfdf5460be5d83a0eecaaa58eaaa7e09e88
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-03-27 00:39:10 -0300

    Project created

---
+++ src/test/java/com/jobson/booksstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobson.booksstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
