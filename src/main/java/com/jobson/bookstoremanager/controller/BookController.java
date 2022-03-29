e7308c463e1fe25c7debd59042ee316a1411169e
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-03-28 23:02:25 -0300

    Changed Controller for example

--- src/main/java/com/jobson/bookstoremanager/controller/BookController.java
+++ src/main/java/com/jobson/bookstoremanager/controller/BookController.java
@@ -17,7 +17,7 @@ public class BookController {
     })
     @GetMapping
     public String hello() {
-        return "Hello Bookstore Manager";
+        return "Hello Bookstore Manager, I am running an example with PR!";
     }
 
 }
