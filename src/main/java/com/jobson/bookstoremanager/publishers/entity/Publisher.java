126f1e129815fe14ca9e7c7ef9d894209b9a64bf
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-03-31 16:44:29 -0300

    Created model mappings

---
+++ src/main/java/com/jobson/bookstoremanager/publishers/entity/Publisher.java
@@ -0,0 +1,33 @@
+package com.jobson.bookstoremanager.publishers.entity;
+
+import com.jobson.bookstoremanager.books.entity.Book;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String code;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate foundationDate;
+
+    @OneToMany(mappedBy = "publihser", fetch = FetchType.LAZY)
+    private List<Book> books;
+}
